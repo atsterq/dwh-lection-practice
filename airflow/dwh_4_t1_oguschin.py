56906cb27ed8176afc6bc2a616d03cefc07c6179
atsterq <atsterq@gmail.com>
2025-03-01 14:54:16 +0500

    task 3

---
+++ airflow/dwh_4_t1_oguschin.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+def generate_data(**kwargs):
+    import random
+
+    import pandas as pd
+
+    data = {
+        "id": range(1, 101),
+        "value": [f"value_{random.randint(1, 100)}" for _ in range(1, 101)],
+    }
+    df = pd.DataFrame(data)
+
+    file_path = "/tmp/data.csv"
+
+    df.to_csv(file_path, index=False)
+
+    kwargs["task_instance"].xcom_push(key="file_path", value=file_path)
+
+
+def load_data(**kwargs):
+    import pandas as pd
+
+    df = pd.read_csv("/tmp/processed_data/data.csv")
+
+    pg_hook = PostgresHook(postgres_conn_id="dwh_4_t1_oguschin_pg")
+
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS test_data (
+        id INTEGER PRIMARY KEY,
+        value VARCHAR(50)
+    );
+    """
+    pg_hook.run(create_table_sql)
+
+    conn = pg_hook.get_conn()
+    df.to_sql("test_data", conn, if_exists="replace", index=False)
+
+    kwargs["task_instance"].xcom_push(key="record_count", value=len(df))
+
+
+def log_record_count(**kwargs):
+    record_count = kwargs["task_instance"].xcom_pull(
+        task_ids="load_data", key="record_count"
+    )
+    print(f"Loaded{record_count} records.")
+
+
+@dag(
+    dag_id="dwh_4_t1_oguschin",
+    schedule_interval=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+)
+def generate_and_load_data():
+    generate = PythonOperator(
+        task_id="generate_data",
+        python_callable=generate_data,
+    )
+
+    move = BashOperator(
+        task_id="move_file",
+        bash_command='mkdir -p /tmp/processed_data && mv {{ task_instance.xcom_pull(task_ids="generate_data", key="file_path") }} /tmp/processed_data/data.csv',
+    )
+
+    load = PythonOperator(
+        task_id="load_data",
+        python_callable=load_data,
+    )
+
+    log = PythonOperator(
+        task_id="log_count",
+        python_callable=log_record_count,
+    )
+
+    generate >> move >> load >> log
+
+
+dag = generate_and_load_data()
